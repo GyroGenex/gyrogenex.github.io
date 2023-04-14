f52e62bf331ceb6514ad99be7325240de945fcd3
yekteng <yektengcheh@gmail.com>
2023-04-14 23:05:26 +0800

    check win lose condition, reset board

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class Card {
+class Card { // card class 
     constructor(face, suit) {
         if (face === "Ace") {
             this.value = 11;
@@ -13,7 +13,8 @@ class Card {
     }
 }
 
-class Deck {
+class Deck { // deck Class. it can shuffle and deal cards
+
     constructor(cardArray) {
         this.cardArray = cardArray;
     }
@@ -59,10 +60,10 @@ $(() => {
 
     const $container = $("div");
 
-    const $computerHand = $("<div>").addClass("computerHand");
+    const $computerHand = $("<div>").addClass("computerHand").attr("countAces", 0);
     const $computerCards = $("<span>").text("Computer's Hand").addClass("description");
     $computerHand.append($computerCards);
-    const $playerHand = $("<div>").addClass("playerHand");
+    const $playerHand = $("<div>").addClass("playerHand").attr("countAces", 0);;
     const $playerCards = $("<span>").text("Players's Hand").addClass("description");
     $playerHand.append($playerCards);
 
@@ -78,19 +79,26 @@ $(() => {
     $hitButton = $("<button>").text("hit").addClass("hit button");
     $choices.append($hitButton);
 
-    $standButton = $("<button>").text("hit").addClass("stand button");
+    $standButton = $("<button>").text("stand").addClass("stand button");
     $choices.append($standButton);
+
+    $nextButton = $("<button>").text("next").addClass("next button");
+    $choices.append($nextButton);
+
 
     const assignCards = (hand) => {
 
         const cardDetails = deck.giveCard();
-        console.log(deck);
-        console.log(cardDetails);
 
         const $card = $("<div>").addClass("card");
         $card.attr("value", cardDetails.value);
         const $cardTop = $("<span>").addClass("top").text(cardDetails.suit);
-        const $cardText = $("<h5>").addClass("center").text(cardDetails.face[0]);
+        const $cardText = $("<h5>").addClass("center");
+        if (cardDetails.face == "10") {
+            $cardText.text(cardDetails.face);
+        } else {
+            $cardText.text(cardDetails.face[0]);
+        }
         const $cardBottom = $("<span>").addClass("bottom").text(cardDetails.suit);
 
         hand.append($card);
@@ -98,11 +106,32 @@ $(() => {
         $card.append($cardText);
         $card.append($cardBottom);
 
-        if (hand.hasClass("computerHand") && hand.children(".card").length === 2) {
-            $card.addClass("card-back");
+        if ($cardText.text() == "A") {
+            console.log("Ace");
+            hand.attr("countaces", parseInt(hand.attr("countaces")) + 1);
         }
 
+        // if (hand.hasClass("computerHand") && hand.children(".card").length === 2) {
+        //     $card.addClass("card-back");
+        // }
+
     };
+
+    const checkValue = (value, hand) => {
+        if (value > 21) {
+            console.log("higher than 21");
+            console.log(hand.attr("countaces"));
+            if (parseInt(hand.attr("countaces")) > 0) {
+                hand.attr("countaces", parseInt(hand.attr("countaces")) - 1);
+                value -= 10;
+                console.log(value);
+                checkValue(value, hand);
+            }
+        }
+
+        return value;
+    };
+
 
     $("button").on("click", (event) => {
         const $button = $(event.currentTarget);
@@ -115,10 +144,68 @@ $(() => {
             $("button").toggleClass("button");
         } else if ($button.hasClass("hit")) {
             assignCards($playerHand);
+        } else if ($button.hasClass("stand")) {
+            let playerValue = 0;
+            const $playerCards = $("div.playerHand").children(".card");
+            $playerCards.each((index, div) => {
+                const value = parseInt($(div).attr("value"));
+                playerValue += value;
+
+            });
+
+
+            let computerValue = 0;
+            const $computerCards = $("div.computerHand").children(".card");
+            $computerCards.each((index, div) => {
+                const value = parseInt($(div).attr("value"));
+                computerValue += value;
+                if ($(div).hasClass("card-back")) {
+                    $(div).toggleClass("card-back");
+                }
+            });
+
+            while (computerValue < 16) {
+                assignCards($computerHand);
+                const $lastCard = $("div.computerHand").children(".card:last-child");
+                computerValue += parseInt($lastCard.attr("value"));
+                computerValue = checkValue(computerValue, $computerHand);
+            }
+
+
+
+            setTimeout(() => {
+                playerValue = checkValue(playerValue, $playerHand);
+                console.log(playerValue);
+                computerValue = checkValue(computerValue, $computerHand);
+                console.log(computerValue);
+
+
+                if (playerValue > 21) {
+                    alert("Player busted");
+                    return;
+                }
+
+                if (computerValue > 21) {
+                    alert("Dealer busted");
+                    return;
+                }
+
+                if (playerValue > computerValue) {
+                    alert("player wins!");
+                } else if (playerValue < computerValue) {
+                    alert("computer wins!");
+                } else {
+                    alert("It's a tie");
+                }
+
+
+            }, 500);
+
+        } else if ($button.hasClass("next")) {
+            $(".card").remove();
+            $("button").toggleClass("button     ");
         }
-
-
-
     });
-})
-    ;
+});
+
+
